a86ffa555d0359a7a8232096b0c9719e920114d4
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-06-23 21:49:02 -0500

    Passport config completed

--- passport-config.js
+++ passport-config.js
@@ -3,9 +3,9 @@ const { authenticate } = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email);
+function initialize(passport, getUserByUsername) {
+  const authenticateUser = async (username, password, done) => {
+    const user = getUserByUsername(username);
     if (user == null) {
       return done(null, false, { message: 'No user with that email' });
     }
@@ -14,13 +14,15 @@ function initialize(passport, getUserByEmail, getUserById) {
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        return done(null, false, { message: 'Password incorrect' });
+        return done(null, false, {
+          message: 'Incorrect Username and/or Password'
+        });
       }
     } catch (e) {
       return done(e);
     }
   };
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.use(new LocalStrategy({ usernameField: authenticateUser }));
   passport.serializeUser((user, done) => done(null, user.id));
   passport.deserializeUser((id, done) => {
     return done(null, getUserById(id));
