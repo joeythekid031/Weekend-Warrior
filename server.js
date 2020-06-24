d11ea4be056ac2b50e6a6f4d8fdb52d81b6ec7ca
Chobu Nkolo-Kangkolo <ckangkolo16@yahoo.com>
2020-06-23 20:11:30 -0500

    fixed epic page

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
-<<<<<<< HEAD
 const path = require('path');
-=======
->>>>>>> 122e6658056b267c6833fa699d3d1d848775f4ce
 
 const initializePassport = require('./passport-config');
 initializePassport(
@@ -60,17 +57,14 @@ app.engine(
   })
 );
 
-<<<<<<< HEAD
 app.set('view engine', 'handlebars');
 // app.set('view engine', 'ejs');
 //app.engine('html', () => {});
 //app.set('views', path.join(__dirname, 'public'));
 //app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'html');
-=======
 //app.set('view engine', 'handlebars');
 app.set('view engine', 'ejs');
->>>>>>> 122e6658056b267c6833fa699d3d1d848775f4ce
 
 // app.get('/', (req, res) => {
 //   res.render('index.handlebars', { name: 'namenamename' });
@@ -81,11 +75,8 @@ app.get('/', checkAuthenticated, (req, res) => {
 });
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
-<<<<<<< HEAD
   res.render('login');
-=======
   res.render('login.ejs');
->>>>>>> 122e6658056b267c6833fa699d3d1d848775f4ce
 });
 
 app.post(
@@ -102,11 +93,8 @@ app.post(
 // });
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
-<<<<<<< HEAD
   res.render('register');
-=======
   res.render('register.ejs');
->>>>>>> 122e6658056b267c6833fa699d3d1d848775f4ce
 });
 
 //app.post('/register', (req, res) => {});
